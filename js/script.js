8a24e1c7f06cb0d0e6bc5c823f0ba784f6293538
cristian <cristiank09@hotmail.com>
2022-07-07 17:14:43 -0500

    yellow

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function elimina(elemento_elim){
+    elemento_elim.remove();
+    console.log(elimina);
+}
+
+function Cambiar(imagen){
+	document.getElementById(imagen).src = 'image/succulents-2.jpg';
+    document.getElementById(imagen).src = 'image/succulents-1.jpg';
+}
